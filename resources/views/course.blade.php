92106c6e3bd708f267fd413f36efbdfc9867771f
Omar Medrano <omar.medrano.ms@gmail.com>
2021-04-12 16:07:37 -0500

    Cursos similares

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -24,12 +24,31 @@
             <img src="{{ $course->image }}" alt="">
             <h2 class="text-4xl">{{ $course->name }}</h2>
             <p>{{ $course->description }}</p>
+
             <div class="flex mt-3">
                 <img src="{{ $course->user->avatar }}" alt="" class="h-10 w-10 rounded-full mr-2">
+
+                <div>
+                    <p class="text-gray-500 text-sm">{{ $course->user->name }}</p>
+                    <p class="text-gray-300 text-xs">{{ $course->created_at->diffForHumans() }}</p>
+                </div>
             </div>
-            <div>
-                <p class="text-gray-500 text-sm">{{ $course->user->name }}</p>
-                <p class="text-gray-300 text-xs">{{ $course->created_at->diffForHumans() }}</p>
+            <div class="grid grid-cols-2 gap-4 my-8">
+                @foreach ($course->similar() as $course)
+                    <div class="bg-white shadow-lg rounded-lg px-4 py-6 text-center">
+                        <a href="{{ route('course', $course->slug) }}">
+                            <img src="{{ $course->image }}" class="rounded-md mb-2">
+                            <h2 class="text-lg text-gray-600 truncate uppercase">
+                                {{ $course->name }}
+                            </h2>
+                            <h3 class="text-md text-gray-500">
+                                {{ $course->excerpt }}
+                            </h3>
+                            <img src="{{ $course->user->avatar }}" class="rounded-full mt-4 mx-auto h-16 w-16">
+                        </a>
+
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
